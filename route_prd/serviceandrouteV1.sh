1ec3560d3b6949390470d554f3fcdae081caf6a5
teerasak.d <teerasak.d@komgrip.com>
2022-06-13 10:39:22 +0700

    auto commit

---
+++ route_prd/serviceandrouteV1.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+echo "Starting Create Service and Routes Version v1 ..."
+echo " > > > Starting Create Service and Routes login ..."
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=Announcement' -d 'url=http://10.19.12.41/api/v1/Announcement'
+curl 127.0.0.1:8001/services/Announcement/routes -d 'paths[]=/api/v1/Announcement'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=announcements' -d 'url=http://10.19.12.41/api/v1/announcement/'
+curl 127.0.0.1:8001/services/announcements/routes -d 'paths[]=/api/v1/announcement/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=login' -d 'url=http://10.19.12.41/api/v1/auth/login'
+curl 127.0.0.1:8001/services/login/routes -d 'paths[]=/api/v1/auth/login'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=refresh' -d 'url=http://10.19.12.41/api/v1/auth/refresh'
+curl 127.0.0.1:8001/services/refresh/routes -d 'paths[]=/api/v1/auth/refresh'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=cameras' -d 'url=http://10.19.12.41/api/v1/cameras'
+curl 127.0.0.1:8001/services/cameras/routes -d 'paths[]=/api/v1/cameras'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=cameras_searchtree' -d 'url=http://10.19.12.41/api/v1/cameras/searchtree'
+curl 127.0.0.1:8001/services/cameras_searchtree/routes -d 'paths[]=/api/v1/cameras/searchtree'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=cameras_tree' -d 'url=http://10.19.12.41/api/v1/cameras/tree'
+curl 127.0.0.1:8001/services/cameras_tree/routes -d 'paths[]=/api/v1/cameras/tree'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=car' -d 'url=http://10.19.12.41/api/v1/car'
+curl 127.0.0.1:8001/services/car/routes -d 'paths[]=/api/v1/car'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=car_type' -d 'url=http://10.19.12.41/api/v1/car/type'
+curl 127.0.0.1:8001/services/car_type/routes -d 'paths[]=/api/v1/car/type'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=car_types' -d 'url=http://10.19.12.41/api/v1/car/type/'
+curl 127.0.0.1:8001/services/car_types/routes -d 'paths[]=/api/v1/car/type/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=image' -d 'url=http://10.19.12.41/api/v1/image/'
+curl 127.0.0.1:8001/services/image/routes -d 'paths[]=/api/v1/image/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=masterdata_highway' -d 'url=http://10.19.12.41/api/v1/masterdata/highway'
+curl 127.0.0.1:8001/services/masterdata_highway/routes -d 'paths[]=/api/v1/masterdata/highway'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=masterdata_provinces' -d 'url=http://10.19.12.41/api/v1/masterdata/provinces'
+curl 127.0.0.1:8001/services/masterdata_provinces/routes -d 'paths[]=/api/v1/masterdata/provinces'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=masterdata_supervisorydivision' -d 'url=http://10.19.12.41/api/v1/masterdata/supervisorydivision'
+curl 127.0.0.1:8001/services/masterdata_supervisorydivision/routes -d 'paths[]=/api/v1/masterdata/supervisorydivision'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=missions' -d 'url=http://10.19.12.41/api/v1/missions'
+curl 127.0.0.1:8001/services/missions/routes -d 'paths[]=/api/v1/missions'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=missions_camera' -d 'url=http://10.19.12.41/api/v1/missions/camera/'
+curl 127.0.0.1:8001/services/missions_camera/routes -d 'paths[]=/api/v1/missions/camera/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=missionss' -d 'url=http://10.19.12.41/api/v1/missions/'
+curl 127.0.0.1:8001/services/missionss/routes -d 'paths[]=/api/v1/missions/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=notifications' -d 'url=http://10.19.12.41/api/v1/notifications'
+curl 127.0.0.1:8001/services/notifications/routes -d 'paths[]=/api/v1/notifications'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=notifications_pushkey' -d 'url=http://10.19.12.41/api/v1/notifications/pushkey'
+curl 127.0.0.1:8001/services/notifications_pushkey/routes -d 'paths[]=/api/v1/notifications/pushkey'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=notifications_read' -d 'url=http://10.19.12.41/api/v1/notifications/read/'
+curl 127.0.0.1:8001/services/notifications_read/routes -d 'paths[]=/api/v1/notifications/read/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=setting' -d 'url=http://10.19.12.41/api/v1/setting'
+curl 127.0.0.1:8001/services/setting/routes -d 'paths[]=/api/v1/setting'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=setting_camera' -d 'url=http://10.19.12.41/api/v1/setting/camera'
+curl 127.0.0.1:8001/services/setting_camera/routes -d 'paths[]=/api/v1/setting/camera'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=setting_profile' -d 'url=http://10.19.12.41/api/v1/setting/profile'
+curl 127.0.0.1:8001/services/setting_profile/routes -d 'paths[]=/api/v1/setting/profile'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=setting_reason' -d 'url=http://10.19.12.41/api/v1/setting/reason'
+curl 127.0.0.1:8001/services/setting_reason/routes -d 'paths[]=/api/v1/setting/reason'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=stations' -d 'url=http://10.19.12.41/api/v1/stations'
+curl 127.0.0.1:8001/services/stations/routes -d 'paths[]=/api/v1/stations'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=notifications_announment' -d 'url=http://10.19.12.41/api/v1/notifications/announment'
+curl 127.0.0.1:8001/services/notifications_announment/routes -d 'paths[]=/api/v1/notifications/announment'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=dlt' -d 'url=http://10.19.12.41/api/v1/dlt/'
+curl 127.0.0.1:8001/services/dlt/routes -d 'paths[]=/api/v1/dlt/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v1' -d 'name=permission' -d 'url=http://10.19.12.41/api/v1/permission/'
+curl 127.0.0.1:8001/services/permission/routes -d 'paths[]=/api/v1/permission'
+sleep 1
---
+++ route_prd/serviceandrouteV2.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+echo "Starting Create Service and Routes Version v2 ..."
+echo " > > > Starting Create Service and Routes login ..."
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=Announcement_v2' -d 'url=http://10.19.12.41/api/v2/Announcement'
+curl 127.0.0.1:8001/services/Announcement_v2/routes -d 'paths[]=/api/v2/Announcement'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=announcements_v2' -d 'url=http://10.19.12.41/api/v2/announcement/'
+curl 127.0.0.1:8001/services/announcements_v2/routes -d 'paths[]=/api/v2/announcement/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=login_v2' -d 'url=http://10.19.12.41/api/v2/auth/login'
+curl 127.0.0.1:8001/services/login_v2/routes -d 'paths[]=/api/v2/auth/login'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=refresh_v2' -d 'url=http://10.19.12.41/api/v2/auth/refresh'
+curl 127.0.0.1:8001/services/refresh_v2/routes -d 'paths[]=/api/v2/auth/refresh'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=cameras_v2' -d 'url=http://10.19.12.41/api/v2/cameras'
+curl 127.0.0.1:8001/services/cameras_v2/routes -d 'paths[]=/api/v2/cameras'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=cameras_searchtree_v2' -d 'url=http://10.19.12.41/api/v2/cameras/searchtree'
+curl 127.0.0.1:8001/services/cameras_searchtree_v2/routes -d 'paths[]=/api/v2/cameras/searchtree'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=cameras_tree_v2' -d 'url=http://10.19.12.41/api/v2/cameras/tree'
+curl 127.0.0.1:8001/services/cameras_tree_v2/routes -d 'paths[]=/api/v2/cameras/tree'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=car_v2' -d 'url=http://10.19.12.41/api/v2/car'
+curl 127.0.0.1:8001/services/car_v2/routes -d 'paths[]=/api/v2/car'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=car_type_v2' -d 'url=http://10.19.12.41/api/v2/car/type'
+curl 127.0.0.1:8001/services/car_type_v2/routes -d 'paths[]=/api/v2/car/type'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=car_types_v2' -d 'url=http://10.19.12.41/api/v2/car/type/'
+curl 127.0.0.1:8001/services/car_types_v2/routes -d 'paths[]=/api/v2/car/type/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=image_v2' -d 'url=http://10.19.12.41/api/v2/image/'
+curl 127.0.0.1:8001/services/image_v2/routes -d 'paths[]=/api/v2/image/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=masterdata_highway_v2' -d 'url=http://10.19.12.41/api/v2/masterdata/highway'
+curl 127.0.0.1:8001/services/masterdata_highway_v2/routes -d 'paths[]=/api/v2/masterdata/highway'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=masterdata_provinces_v2' -d 'url=http://10.19.12.41/api/v2/masterdata/provinces'
+curl 127.0.0.1:8001/services/masterdata_provinces_v2/routes -d 'paths[]=/api/v2/masterdata/provinces'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=masterdata_supervisorydivision_v2' -d 'url=http://10.19.12.41/api/v2/masterdata/supervisorydivision'
+curl 127.0.0.1:8001/services/masterdata_supervisorydivision_v2/routes -d 'paths[]=/api/v2/masterdata/supervisorydivision'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=missions_v2' -d 'url=http://10.19.12.41/api/v2/missions'
+curl 127.0.0.1:8001/services/missions_v2/routes -d 'paths[]=/api/v2/missions'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=missions_camera_v2' -d 'url=http://10.19.12.41/api/v2/missions/camera/'
+curl 127.0.0.1:8001/services/missions_camera_v2/routes -d 'paths[]=/api/v2/missions/camera/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=missionss_v2' -d 'url=http://10.19.12.41/api/v2/missions/'
+curl 127.0.0.1:8001/services/missionss_v2/routes -d 'paths[]=/api/v2/missions/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=notifications_v2' -d 'url=http://10.19.12.41/api/v2/notifications'
+curl 127.0.0.1:8001/services/notifications_v2/routes -d 'paths[]=/api/v2/notifications'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=notifications_pushkey_v2' -d 'url=http://10.19.12.41/api/v2/notifications/pushkey'
+curl 127.0.0.1:8001/services/notifications_pushkey_v2/routes -d 'paths[]=/api/v2/notifications/pushkey'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=notifications_read_v2' -d 'url=http://10.19.12.41/api/v2/notifications/read/'
+curl 127.0.0.1:8001/services/notifications_read_v2/routes -d 'paths[]=/api/v2/notifications/read/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=setting_v2' -d 'url=http://10.19.12.41/api/v2/setting'
+curl 127.0.0.1:8001/services/setting_v2/routes -d 'paths[]=/api/v2/setting'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=setting_camera_v2' -d 'url=http://10.19.12.41/api/v2/setting/camera'
+curl 127.0.0.1:8001/services/setting_camera_v2/routes -d 'paths[]=/api/v2/setting/camera'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=setting_profile_v2' -d 'url=http://10.19.12.41/api/v2/setting/profile'
+curl 127.0.0.1:8001/services/setting_profile_v2/routes -d 'paths[]=/api/v2/setting/profile'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=setting_reason_v2' -d 'url=http://10.19.12.41/api/v2/setting/reason'
+curl 127.0.0.1:8001/services/setting_reason_v2/routes -d 'paths[]=/api/v2/setting/reason'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=stations_v2' -d 'url=http://10.19.12.41/api/v2/stations'
+curl 127.0.0.1:8001/services/stations_v2/routes -d 'paths[]=/api/v2/stations'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=notifications_announment_v2' -d 'url=http://10.19.12.41/api/v2/notifications/announment'
+curl 127.0.0.1:8001/services/notifications_announment_v2/routes -d 'paths[]=/api/v2/notifications/announment'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=dlt_v2' -d 'url=http://10.19.12.41/api/v2/dlt/'
+curl 127.0.0.1:8001/services/dlt_v2/routes -d 'paths[]=/api/v2/dlt/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=permission_v2' -d 'url=http://10.19.12.41/api/v2/permission/'
+curl 127.0.0.1:8001/services/permission_v2/routes -d 'paths[]=/api/v2/permission'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=blacklist0' -d 'url=http://10.19.12.41/api/v2/blacklist/'
+curl 127.0.0.1:8001/services/blacklist0/routes -d 'paths[]=/api/v2/blacklist/'
+sleep 1
+
+curl 127.0.0.1:8001/services -d 'tags=v2' -d 'name=blacklist' -d 'url=http://10.19.12.41/api/v2/blacklist'
+curl 127.0.0.1:8001/services/blacklist/routes -d 'paths[]=/api/v2/blacklist'
+sleep 1
