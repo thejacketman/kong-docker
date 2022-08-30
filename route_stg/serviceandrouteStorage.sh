e1fe6b97878b4b22c828baa17593ec4d4d4f1fa9
teerasak <teerasak.d@komgrip.com>
2022-08-30 15:58:14 +0700

    auto commit

---
+++ route_stg/serviceandrouteStorage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Starting Create Service and Routes Version v ..."
+echo " > "
+
+curl 127.0.0.1:8001/services -d 'name=storage' -d 'url=http://10.19.12.141/api/storage'
+curl 127.0.0.1:8001/services/storage/routes -d 'paths[]=/api/storage'
+sleep 1
